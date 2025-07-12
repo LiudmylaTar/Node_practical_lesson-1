0e50e6343a826268709102f61b8a47aa15464b18
Liudmyla_Taran <taran.lyudok@gmail.com>
2025-07-12 21:40:49 +0300

    finish

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,4 +1,11 @@
-import { getProducts } from "../services/products.js";
+import createHttpError from "http-errors";
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../services/products.js";
 
 export const getProductsController = async (req, res) => {
   const products = await getProducts();
@@ -7,4 +14,48 @@ export const getProductsController = async (req, res) => {
     message: "Successfully found products!",
     data: products,
   });
+};
+
+export const getProductController = async (req, res) => {
+  const { productId } = req.params;
+  const product = await getProductById(productId);
+  if (!product) {
+    throw createHttpError(404, "Contact not found");
+  }
+  res.json({
+    status: 200,
+    message: `Successfully found product with id ${productId}!`,
+    data: product,
+  });
+};
+
+export const createProductController = async (req, res) => {
+  const result = await createProduct(req.body);
+  res.status(201).json({
+    status: 201,
+    message: "Successfully created a product!",
+    data: result,
+  });
+};
+
+export const updateProductController = async (req, res, next) => {
+  const result = await updateProduct(req.params.productId, req.body);
+  if (!result) {
+    next(createHttpError(404, "Product not found"));
+    return;
+  }
+  res.status(200).json({
+    status: 200,
+    message: "Successfully patched a product!",
+    data: result,
+  });
+};
+
+export const deleteProductController = async (req, res, next) => {
+  const result = await deleteProduct(req.params.productId);
+  if (!result) {
+    next(createHttpError(404, "Product not found"));
+    return;
+  }
+  res.status(204).send();
 };
--- src/routers/products.js
+++ src/routers/products.js
@@ -1,10 +1,24 @@
 import { Router } from "express";
 
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { getProductsController } from "../controllers/products.js";
+import {
+  getProductsController,
+  getProductController,
+  createProductController,
+  updateProductController,
+  deleteProductController,
+} from "../controllers/products.js";
 
 const productsRouter = Router();
 
 productsRouter.get("/", ctrlWrapper(getProductsController));
+
+productsRouter.get("/:productId", ctrlWrapper(getProductController));
+
+productsRouter.post("/", ctrlWrapper(createProductController));
+
+productsRouter.patch("/:productId", ctrlWrapper(updateProductController));
+
+productsRouter.delete("/:productId", ctrlWrapper(deleteProductController));
 
 export default productsRouter;
